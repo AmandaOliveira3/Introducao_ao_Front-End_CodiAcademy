666886fca8798a0669944af8672eaae6da0d64a4
AmandaOliveira3 <amanda.oliveira@unesp.br>
2023-03-01 19:44:14 -0300

    exercicios de Front-End

---
+++ Javascrpit/IF e ELSE/Exercicio.js
@@ -0,0 +1,13 @@
+let nota = 75;
+
+
+
+if(nota > 40 && nota < 60){
+    alert("aprovado");
+}
+else if(60>= 100){
+    alert("pode fazer prova de recuperação");
+}
+else{
+    alert("reprovado");
+}
---
+++ Javascrpit/IF e ELSE/IF e ELSE.js
@@ -0,0 +1,13 @@
+let vel;
+
+vel= 100;
+
+if(vel > 0 && vel < 100){
+    alert("andando");
+}
+else if(vel>= 100){
+    alert("correndo");
+}
+else{
+    alert("parado");
+}
